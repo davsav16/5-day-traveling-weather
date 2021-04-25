355ceb72cf18920700e4eee90b2fa0ffdfa39658
Davsav <davidbyu6@gmail.com>
2021-04-24 21:31:49 -0600

    added UVI index

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 var cityArr = [];
+var ulHistory= document.querySelector("#list-cityOutput");
+
+
 
 var displayCity = function(city) {
-    var history = document.querySelector("#list-cityOutput");
     var cityLi = document.createElement('li')
     var cityBtn = document.createElement("button");
-    var text = document.createTextNode(city)
-    text.classList.add("")
+    var text = document.createElement('p')
+    text.innerText = city;
+    text.classList.add("sBtnText");
     cityBtn.classList.add("searchBtn");
+    cityBtn.addEventListener('click', sBtnClick)
     cityBtn.appendChild(text);
-    history.appendChild(cityLi);
     cityLi.appendChild(cityBtn);
+    ulHistory.appendChild(cityLi);
 }
 
 //var t = document.createTextNode("test content");
 //b.appendChild(t);
 
-var loadCity = function(codeName) {
+var loadCity = function() {
     if (!JSON.parse(localStorage.getItem("city"))) {
         return;
     } else {
-        if (codeName === 'clicked') {
-            var children = document.querySelector('#list-cityOutput').children
-            children.forEach(child => {
-                document.querySelector('#list-cityOutput').removeChild(child);
-            });            
-        }
         cityArr = JSON.parse(localStorage.getItem("city"));
         cityArr.forEach(city => {
             displayCity(city);
@@ -32,18 +30,22 @@ var loadCity = function(codeName) {
     };
 
 }
-loadCity('loaded');
+loadCity();
 
 var saveCity = function(city) {
     cityArr.push(city);
     localStorage.setItem("city", JSON.stringify(cityArr));
+    var parent = document.querySelector('#list-cityOutput')
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
   };
 
 function currentDate() {
     document.querySelector("#currentDay").innerHTML = moment().format("l"); 
-    for(var i = 1; i<6; i++){
-    document.querySelector(`#day${i}date`).innerHTML = moment().add(i, 'd').format("l");
-    }
+        for(var i = 1; i<6; i++){
+            document.querySelector(`#day${i}date`).innerHTML = moment().add(i, 'd').format("l");
+        }
     }
 currentDate();
 
@@ -53,9 +55,13 @@ let weather = {
         fetch ("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=9e8297a7de594ed3964b6416e7efecc3")
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            this.displayWeather(data)
-            this.fiveDay(city);
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=hourly&appid=9e8297a7de594ed3964b6416e7efecc3`)
+            .then(response => response.json())
+            .then(uviData => {
+                var newData = { ...data, extraData: uviData }
+                this.displayWeather(newData)
+                this.fiveDay(city);
+            })
         });
     },
     displayWeather: function(data) {
@@ -63,12 +69,14 @@ let weather = {
         const { icon } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
+        const { extraData } = data;
 
         document.querySelector(".city").innerHTML = name;
         document.querySelector("#icon").setAttribute('src', `http://openweathermap.org/img/wn/${icon}.png`);
         document.querySelector(".temp").innerHTML = temp + " °F";
         document.querySelector(".wind").innerHTML = speed + " mph";
         document.querySelector(".humidity").innerHTML = humidity + "%";
+        document.querySelector(".uv").innerHTML = `UV Index: ${extraData.current.uvi}`;
 
 
     },
@@ -87,7 +95,6 @@ let weather = {
         fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${this.apiKey}`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             this.display5Day(data);
         });
     },
@@ -102,14 +109,17 @@ let weather = {
 
 
 
+
 document.querySelector("#form").addEventListener("submit", function(event) {
-    console.log(event.target[0].value)
     event.preventDefault();
     weather.search();
     saveCity(event.target[0].value);
-    loadCity('clicked');
+    loadCity();
     
 });
 
 
-
+function sBtnClick(event) {
+    weather.fetchWeather(event.target.innerText)
+    
+}
